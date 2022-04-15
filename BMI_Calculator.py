68fdeca72784a986786c0290dd47f247749ac2d4
Lokesh Choudhary Medharametla <73935267+Lokesh-Choudhary@users.noreply.github.com>
2022-04-15 19:14:53 +0530

    BMI_Calculator.py

---
+++ BMI_Calculator.py
@@ -0,0 +1,15 @@
+
+#BMI_CALCULATOR
+weight=int(input())
+height=float(input())
+BMI=weight/float(height*height)
+if BMI<18.5:
+   print("Underweight")
+
+elif BMI>=18.5 and BMI<25:
+    print("Normal")
+
+elif BMI>=25 and BMI<30:
+    print("Overweight")
+else:
+    print("Obesity")
