d113f2f88db4c72941ee1ac024554778d165ebb3
Lokesh Choudhary Medharametla <73935267+Lokesh-Choudhary@users.noreply.github.com>
2022-04-28 18:49:33 +0530

    Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,7 @@
+a=input(("Enter a letter:"))  
+print("Reverse is ",a[::-1])
+
+if(a==a[::-1]):  
+      print("The letter is a palindrome")  
+else:  
+      print("The letter is not a palindrome")  
